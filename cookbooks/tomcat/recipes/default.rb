33751b4f2d349113c3a15f738ebca1a439655978
Atishay <atishayshukla@gmail.com>
2016-07-20 21:25:55 +0530

    Recipe updated with Java package

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+# execute "java-1.7.0-openjdk-devel"
+
+package 'java-1.7.0-openjdk-devel' do
+  action :install
+end
+
