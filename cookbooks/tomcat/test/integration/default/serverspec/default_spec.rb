61af6de08a6beb3048a2796cab714c32b18203de
Atishay <atishayshukla@gmail.com>
2016-07-20 21:20:15 +0530

    update kitchen test for java package

--- cookbooks/tomcat/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/tomcat/test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,9 @@ describe 'tomcat::default' do
   	describe command('curl http://localhost:8080') do
   		its(:stdout) { should match /Tomcat/ }
   	end
+
+  	describe package 'java-1.7.0-openjdk-devel' do
+  	  it { should be_installed }
+  	end
+  	
 end
