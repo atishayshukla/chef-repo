6d0a528c2ff5472b84c50af041ee4ce4fa853a23
Atishay <atishayshukla@gmail.com>
2016-07-22 21:44:05 +0530

    made the version variable in unit test

--- cookbooks/redis/spec/unit/recipes/default_spec.rb
+++ cookbooks/redis/spec/unit/recipes/default_spec.rb
@@ -13,6 +13,10 @@ describe 'redis::default' do
       runner.converge(described_recipe)
     end
 
+    # In Test also we can make the variable at one place
+
+    let(:version) { '2.8.9' }
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
@@ -27,20 +31,20 @@ describe 'redis::default' do
     end
 
     it 'retrives the application from source' do
-      expect(chef_run).to create_remote_file('/tmp/redis-2.8.9.tar.gz')
+      expect(chef_run).to create_remote_file('/tmp/redis-#{version}.tar.gz')
     end
 
     it 'unzips the application' do
-      # expect(chef_run).to run_execute('tar xzf /tmp/redis-2.8.9.tar.gz')
+      # expect(chef_run).to run_execute('tar xzf /tmp/redis-#{version}.tar.gz')
       # This expectation is going to fail as in recipe
       # This action does nothing so we will look in below how to implement it
-      resource = chef_run.remote_file('/tmp/redis-2.8.9.tar.gz')
-      expect(resource).to notify('execute[tar xzf redis-2.8.9.tar.gz]').to(:run).immediately
+      resource = chef_run.remote_file('/tmp/redis-#{version}.tar.gz')
+      expect(resource).to notify('execute[tar xzf redis-#{version}.tar.gz]').to(:run).immediately
       # This is how notify works
     end
 
     it 'builds and installs the application' do
-      resource = chef_run.execute('tar xzf redis-2.8.9.tar.gz')
+      resource = chef_run.execute('tar xzf redis-#{version}.tar.gz')
       expect(resource).to notify('execute[make && make install]').to(:run).immediately
     end
 
