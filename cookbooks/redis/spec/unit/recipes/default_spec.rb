27c1c7f588e98cf60d94a15d23535f992fd670c4
atishayshukla <atishayshukla@gmail.com>
2016-07-22 13:09:26 +0530

    Update default_spec.rb

--- cookbooks/redis/spec/unit/recipes/default_spec.rb
+++ cookbooks/redis/spec/unit/recipes/default_spec.rb
@@ -18,7 +18,7 @@ describe 'redis::default' do
     end
 
     it 'updates the package repository' do
-      expect(chef_run).to execute[apt-get update]
+      expect(chef_run).to run_execute('apt-get update')
     end
 
     it 'installs the necessary packages'
